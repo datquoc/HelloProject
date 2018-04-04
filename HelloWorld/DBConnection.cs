24419a6fdefebf9eed772292e9692fc73b8ea4a7
ADMIN\Admin <dattqgc00922@fpt.edu.vn>
2018-04-04 09:13:59 +0700

    Initialize Project

---
+++ HelloWorld/DBConnection.cs
@@ -0,0 +1,115 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class DBConnection
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public DBConnection()
+        {
+            Initialize();
+        }
+
+
+
+        //Initialize values
+        private void Initialize()
+        {
+            server = "localhost:3306";
+            database = "student";
+            uid = "root";
+            password = "";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Error");
+                        break;
+
+                    case 1045:
+                        Console.WriteLine("Error");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Error");
+                return false;
+            }
+        }
+
+        public bool Insert(Student student)
+        {
+            string query = "INSERT INTO students (id,name, rollNumber) VALUES("+ student.Id +",'"+ student.Name +"', '"+ student.RollNumber +"')";
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                try
+                {
+                    //create command and assign the query and connection from the constructor
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                    //Execute command
+                    cmd.ExecuteNonQuery();
+
+                    //close connection
+                    this.CloseConnection();
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Error " + ex.Message);
+                }
+            }
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            DBConnection p = new DBConnection();
+            Student st = new Student(2,"Ta Quoc Dat","ABCDEF");
+            p.Insert(st);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HelloWorld/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Student
+    {
+        private List<Student> list = new List<Student>(); 
+        private int id;
+        private string name;
+        private string rollNumber;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string RollNumber { get => rollNumber; set => rollNumber = value; }
+
+        public Student(int id, string name, string rollNumber)
+        {
+            this.id = id;
+            this.name = name;
+            this.rollNumber = rollNumber;
+        }
+
+        public override string ToString()
+        {
+            return this.id + " - " + this.name + " - " + this.rollNumber;
+        }
+
+        public Student()
+        {
+        }
+
+        public Student(string name, string rollNumber)
+        {
+            this.name = name;
+            this.rollNumber = rollNumber;
+        }
+    }
+}
